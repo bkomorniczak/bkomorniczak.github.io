10786de43e3507449ab5d4ab78bc99d67c8df53d
bkomorniczak <b.komorniczak@gmail.com>
2020-09-24 13:40:12 +0200

    Typo fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://dry-inlet-12045.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://dry-inlet-12045.herokuapp.com/v1/trello';
+  const trelloApiRoot = 'https://dry-inlet-12045.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
